6920073a5e91a5463b239f546d577edc860e6df8
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2024-07-18 00:15:41 +0500

    refactor: Improve landing page banner and product page layout

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -15,25 +15,17 @@ export default function Products() {
       <main className="flex flex-col lg:flex-row justify-center items-center lg:space-x-10 p-5 px-10">
         <div className="flex flex-col justify-start items-center">
           <h1>
-            <p className="lg:text-7xl md:text-6xl text-5xl font-bold">
-              Way Point{" "}
-            </p>
-            <span className="lg:text-5xl md:text-4xl text-3xl font-bold">
-              Warranty{" "}
-            </span>
-            <span className="lg:text-5xl md:text-4xl text-3xl font-bold text-blue">
-              Products
-            </span>
+            <p className="lg:text-7xl md:text-6xl text-5xl font-bold">Waypoint </p>
+            <span className="lg:text-5xl md:text-4xl text-3xl font-bold">Warranty </span>
+            <span className="lg:text-5xl md:text-4xl text-3xl font-bold text-blue">Products</span>
           </h1>
           <div className="bg-lightBlue items-center md:items-end lg:w-[500px] p-6 rounded-xl my-8 flex justify-start lg:items-start flex-col">
             <p className="lg:pr-20 lg:text-left text-center text-sm lg:text-base">
-              Total Warranty Services specializes in customized service
-              contracts, protection and maintenance plans, and a wide range of
-              ancillary products to meet your customers&apos; needs. We can
-              provide almost any variation of these products, which are
-              tried-and-true marketing tools that can help boost your bottom
-              line.Our goal was to transform the automotive consumer experience
-              by enhancing products and creating a roadmap for what&apos;s next.
+              Total Warranty Services specializes in customized service contracts, protection and maintenance plans, and
+              a wide range of ancillary products to meet your customers&apos; needs. We can provide almost any variation
+              of these products, which are tried-and-true marketing tools that can help boost your bottom line.Our goal
+              was to transform the automotive consumer experience by enhancing products and creating a roadmap for
+              what&apos;s next.
             </p>
             <button
               onClick={() => router.push("/products#products")}
@@ -45,11 +37,7 @@ export default function Products() {
         </div>
         <div className="relative w-[90vw] lg:w-[50vw] h-[60vh] p-10 ">
           <div>
-            <Image
-              src={Image3}
-              alt="Car Image"
-              className="object-cover h-[50vh]"
-            />
+            <Image src={Image3} alt="Car Image" className="object-cover h-[50vh]" />
           </div>
           <svg
             width="29"
@@ -59,10 +47,7 @@ export default function Products() {
             xmlns="http://www.w3.org/2000/svg"
             className="absolute top-0 left-0"
           >
-            <path
-              d="M0.000125885 16.2792L28.1965 6.41938e-05L16.2793 44.4756L0.000125885 16.2792Z"
-              fill="#4D9CFF"
-            />
+            <path d="M0.000125885 16.2792L28.1965 6.41938e-05L16.2793 44.4756L0.000125885 16.2792Z" fill="#4D9CFF" />
           </svg>
           <svg
             width="30"
@@ -72,10 +57,7 @@ export default function Products() {
             xmlns="http://www.w3.org/2000/svg"
             className="absolute bottom-0 left-0"
           >
-            <path
-              d="M29.371 29.6067L0.393245 37.3713L21.6064 0.628906L29.371 29.6067Z"
-              fill="#4D9CFF"
-            />
+            <path d="M29.371 29.6067L0.393245 37.3713L21.6064 0.628906L29.371 29.6067Z" fill="#4D9CFF" />
           </svg>
           <svg
             width="40"
@@ -107,15 +89,10 @@ export default function Products() {
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-[2] opacity-35"></div>
         <div className="container z-[3] flex h-full items-center text-center flex-col text-primaryText px-4 pt-11">
-          <h1 className="md:text-5xl text-4xl lg:text-7xl font-semibold">
-            Products
-          </h1>
+          <h1 className="md:text-5xl text-4xl lg:text-7xl font-semibold">Products</h1>
         </div>
       </section>
-      <section
-        id="products"
-        className="bg-secondaryBg flex justify-center items-center py-5 px-10"
-      >
+      <section id="products" className="bg-secondaryBg flex justify-center items-center py-5 px-10">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 md:col-span-1 md:gap-x-16 lg:gap-x-10 lg:gap-y-10 md:gap-y-6 gap-y-5 lg:col-span-1 my-12">
           {productDetails.map((product, index: number) => (
             <ProductCard
